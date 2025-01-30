406a23998f397307f21924d98afcf86000302d8b
Daniel Corcoran <corcordan@gmail.com>
2025-01-30 17:03:42 -0500

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static export
-  trailingSlash: true, // Optionally add trailing slashes to URLs
 };
 
 export default nextConfig;
