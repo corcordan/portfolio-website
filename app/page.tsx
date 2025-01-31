84938c794499556318fac617001b88f6e8ed9817
Daniel Corcoran <corcordan@gmail.com>
2025-01-31 16:51:39 -0500

    Removed Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Resume from "@/components/Resume";
