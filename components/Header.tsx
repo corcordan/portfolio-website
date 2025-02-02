0504a064b068fef511075b0aed159f23922d88af
Daniel Corcoran <corcordan@gmail.com>
2025-02-01 23:55:41 -0500

    Headshot

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
@@ -5,8 +6,9 @@ const Header = () => {
     <header className="flex flex-row justify-between py-5 px-10 bg-blue-950 flex-none">
       <div className="text-4xl font-bold text-blue-300">|corcordan|</div>
       <div className="flex flex-row text-lg gap-5 items-center text-blue-300 font-bold">
+          
           <div className="">Home</div>
-          <div className="">About</div>
+          <Link href="/About">About</Link>
           <div>Resume</div>
           <div>Projects</div>
           <div>Contact</div>
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Disables the Next.js image optimization
+  },
 };
 
 export default nextConfig;
